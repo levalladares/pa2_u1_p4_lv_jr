073c6d8a26582701c81f746f2e60489857cb7007
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-09 19:31:45 -0500

    mi primera subida

---
+++ src/main/java/com/example/demo/Pa2U1P4JrLvApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P4JrLvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P4JrLvApplication.class, args);
+	}
+
+}
