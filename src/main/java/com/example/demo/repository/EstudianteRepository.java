441695755c58f03107160c5153d61a9e019824ba
levalladares <levalladares@uce.edu.ec>
2023-05-19 19:42:50 -0500

    Taller6

---
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+public interface EstudianteRepository {
+	public void insertar(Estudiante estudiante);
+
+	public void actualizar(Estudiante estudiante);
+
+	// buscamos por un identificador
+	public Estudiante seleccionar(String cedula);
+
+	// eliminamos por un identificador
+	public void eliminar(String cedula);
+}
